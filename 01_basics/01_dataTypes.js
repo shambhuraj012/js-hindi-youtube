5430af10bd9c731bcf010ef79e41af8dfc591a7c
shambhuraj012 <shambhurajshinde1211@gmail.com>
2026-07-02 18:28:15 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"   // treat all cde as the newwer version 
+
+// alert (3 + 30 ) we are using node js not browser
+
+console.log(3 + 
+  3) // code relability should be high 
+console.log("shambhuraj")
+
+
+let name = "shambhuraj"
+let age = 21
+let isLoggedIn = false;
+// let = State;
+
+// number => 2 to power 53
+// bigint
+// string => "shambhuraj"
+// boolean  => true / false
+// null => standalone value
+// undefind => 
+
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null) // object
