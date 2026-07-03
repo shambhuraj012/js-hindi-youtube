59691e668f797f08266dff3c92c81105e0c40a70
shambhuraj012 <shambhurajshinde1211@gmail.com>
2026-07-03 17:33:23 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+console.log(2>3) 
+console.log(2=3)
+console.log(2>=3)
+console.log(2==3)
+console.log(2>!3)
+
+
+console.log("3"> 2)         //this type OF operator you naver use it is vary difficult to understand
+console.log("23"> 23)
+
+console.log(null > 0)        //this type OF operator you naver use it is vary difficult to understand
+console.log(null == 0)
+console.log(null >= 0)
+
+
+console.log(undefined = 0)      //this type OF operator you naver use it is vary difficult to understand
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+
