ea65b9a6132a68dbf5db90b9228e149b192c416f
shambhuraj012 <shambhurajshinde1211@gmail.com>
2026-07-08 18:01:41 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,36 @@
+// array
+
+
+ const array = [1,2,3,4,5]
+// //console.log(array);
+
+ const myarr2 = new Array(1,2,3,4)
+// console.log(myarr2);
+
+
+// array methods 
+
+// myarr2.push(5)
+// console.log(myarr2);
+
+// myarr2.pop()
+// console.log(myarr2);
+
+
+// myarr2.unshift(4)
+// console.log(myarr2);
+
+// myarr2.shift()
+// console.log(myarr2);
+
+
+// console.log(myarr2.includes(2));
+// console.log(myarr2.indexOf(3))
+// console.log(myarr2);
+
+const newarr =  myarr2.join()
+
+console.log(myarr2);
+console.log(array);
+
+console.log(newarr);
